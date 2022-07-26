b52d1814f52d3fad603664b9cf5c7764eff20ba4
James Alhassan <jamesalhassan052@gmail.com>
2022-07-26 22:36:24 +0100

    wildcard . and g flag

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ let result2 = fccRegex.test(myString);
 
 "Hello, World!".match(/Hello/);
 let ourStr = "Regular expressions";
-let ourRegex = /expressions/;
-ourStr.match(ourRegex);
+let ourRegex1 = /expressions/;
+ourStr.match(ourRegex1);
 // Here the first match would return ["Hello"] and the second would return ["expressions"].
 
 // Note that the .match syntax is the "opposite" of the .test method you have been using thus far:
@@ -79,4 +79,45 @@ ourStr.match(ourRegex);
 
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
+let result3 = extractStr.match(codingRegex); // Change this line
+
+// Find More Than the First Match
+// So far, you have only been able to extract or search a pattern once.
+
+let testStr4 = "Repeat, Repeat, Repeat";
+let ourRegex = /Repeat/;
+testStr4.match(ourRegex);
+// Here match would return ["Repeat"].
+
+// To search or extract a pattern more than once, you can use the global search flag: g.
+
+let repeatRegex = /Repeat/g;
+testStr4.match(repeatRegex);
+// And here match returns the value ["Repeat", "Repeat", "Repeat"]
+
+// Using the regex starRegex, find and extract both Twinkle words from the string twinkleStar.
+
+// Note
+// You can have multiple flags on your regex like /search/gi
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result5 = twinkleStar.match(starRegex); // Change this line
+
+// Match Anything with Wildcard Period
+// Sometimes you won't (or don't need to) know the exact characters in your patterns. Thinking of all words that match, say, a misspelling would take a long time. Luckily, you can save time using the wildcard character: .
+
+// The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(humStr);
+huRegex.test(hugStr);
+// Both of these test calls would return true.
+
+// Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/g; // Change this line
+let result = unRegex.test(exampleStr);
